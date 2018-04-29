119a43e5239092073e231342f7771f8b537e1d95
Brian Berg <brianberg.se@gmail.com>
2018-04-29 15:17:50 -0500

    style(PokemonEvolution): add candy and distance costs
     - update arrow
     - add shift and highlighting

--- src/components/PokemonEvolution/components/EvolutionTrack/index.tsx
+++ src/components/PokemonEvolution/components/EvolutionTrack/index.tsx
@@ -6,38 +6,76 @@ import "./styles.scss";
 
 export class EvolutionTrack extends React.Component<any, {}> {
 
-  familyId : number;
-  track    : any[];
+  familyId  : number;
+  track     : any[];
+  shift     : number;
+  highlight : number;
 
   constructor(props : any) {
     super(props);
-    this.familyId = props.familyId;
-    this.track    = props.track;
+    this.familyId  = props.familyId;
+    this.track     = props.track;
+    this.shift     = props.shift;
+    this.highlight = props.highlight;
   }
 
   render() {
-    const imageSize  = 40;
-    const index      = this.track[0].id;
-    const evolutions = this.track.map((evolution) => {
-      return (
-        <div key={evolution.evolution} className="evolution">
-          <div className="evolution__arrow">--></div>
-          <PokemonImage
-              pokemonId={evolution.evolution}
-              width={imageSize}
-              height={imageSize}
-              />
+    const imageSize = 56;
+    const index     = this.track[0].id;
+    const items     = [
+      <div key={'p' + index} 
+          className={index === this.highlight ? "highlight" : ''}>
+        <PokemonImage
+          pokemonId={index}
+          width={imageSize}
+          height={imageSize}
+        />
+      </div>
+    ];
+    // Add each evolution in the track
+    for (let i in this.track) {
+      const item = this.track[i];
+      const distance = item.km_buddy_distance_requirement;
+      // Evolution elements
+      const candyElement = (
+        <div className="evolution__candy">
+          {item.candy_cost}
         </div>
       );
-    });
-    return (
-      <div className="evolution-track">
-        <PokemonImage
-            pokemonId={index}
+      const distanceElement = (
+        <div className="evolution__distance">
+          {distance > 0 ? distance + " km" : ''}
+        </div>
+      );
+      // Add evolution
+      items.push(
+        <div key={'e' + i} className="evolution">
+          {candyElement}
+          <i className="material-icons">keyboard_backspace</i>
+          {distanceElement}
+        </div>
+      );
+      // Add next pokemon image
+      items.push(
+        <div key={'p' + (i + 1)}
+            className={item.evolution === this.highlight ? "highlight" : ''}>
+          <PokemonImage
+            pokemonId={item.evolution}
             width={imageSize}
             height={imageSize}
-            />
-        {evolutions}
+          />
+        </div>
+      );
+    }
+    // Shift track a certain number of pokemon evolution widths
+    const styles : any   = {};
+    if (this.shift) {
+      const evolutionWidth = 172;
+      styles.marginLeft = '' + (evolutionWidth * this.shift) + "px";
+    }
+    return (
+      <div className="evolution-track" style={styles}>
+        {items}
       </div>
     );
   }
--- src/components/PokemonEvolution/index.tsx
+++ src/components/PokemonEvolution/index.tsx
@@ -5,34 +5,45 @@ import PokedexService from "../../services/Pokedex";
 import { PokemonImage }   from "../PokemonImage";
 import { EvolutionTrack } from "./components/EvolutionTrack";
 
+import "./styles.scss";
+
 export class PokemonEvolution extends React.Component<any, {}> {
 
-  familyId : number;
-  tracks   : any[];
+  familyId  : number;
+  tracks    : any[];
+  highlight : number;
 
   constructor(props : any) {
     super(props);
-    const Pokedex = PokedexService.getInstance();
-    this.familyId = props.familyId;
-    this.tracks = Pokedex.getFamily(this.familyId);
+    const Pokedex  = PokedexService.getInstance();
+    this.familyId  = props.familyId;
+    this.tracks    = Pokedex.getFamily(this.familyId);
+    this.highlight = props.highlight;
   }
 
   render() {
-    if (!this.tracks.length) {
-      const imageSize = 40;
+    const tracks = this.tracks.map((track, index) => {
+      const styles : any = {};
+      const diff = index > 0 ? this.tracks[index - 1].length - track.length : 0;
       return (
+        <EvolutionTrack
+            key={index}
+            track={track}
+            shift={diff}
+            highlight={this.highlight}
+            />
+      );
+    });
+    if (tracks.length === 0) {
+      const imageSize = 56;
+      tracks.push(
         <PokemonImage
+          key={0}
           pokemonId={this.familyId}
           width={imageSize}
           height={imageSize}
-          />
-      );
+        />)
     }
-    const tracks = this.tracks.map((track, index) => {
-      return (
-        <EvolutionTrack key={index} track={track} />
-      );
-    });
     return (
       <div className="evolutions">
         {tracks}
